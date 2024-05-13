d3bc494fd9dc23dbdf9a3f0c98cbf741904e30a4
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-05-13 20:09:22 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+    class Q2
+    {
+        public static void main (String[] args)
+        {
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            // write your code here
+            if( x< y ){
+                System.out.println("yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+    }
+
