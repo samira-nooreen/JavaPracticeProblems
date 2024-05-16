1dc45750d8ca9a32b0de7b0535ea60053e9f9bb0
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-05-16 18:54:00 +0530

    Create Q18.java

---
+++ Q18.java
@@ -0,0 +1,7 @@
+public class Q18 {
+    public static void main(String[] args) {
+        //write your code here
+      System.out.println("Amigo");
+      System.out.println("10/18/2012");
+    }
+}
