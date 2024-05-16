4dd740f6e96de198682430187042708fd247d7c5
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-05-16 19:07:07 +0530

    Create Q20.java

---
+++ Q20.java
@@ -0,0 +1,16 @@
+
+public class Q20 {
+    public static void main(String[] args) {
+       String name = "Amigo";
+     //  int day = 18;
+       int  age = 10;
+    //   int  year = 2012;
+     //   String month = "10";
+        String myNameIs = "My name is ";
+      // birthDate = day + month + year;
+        String iAm = "I am ";
+
+        System.out.println(myNameIs + name);
+        System.out.println(iAm + age);
+    }
+}
