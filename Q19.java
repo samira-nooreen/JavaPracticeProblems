6472ad5605010f1f0c3ec32bbbb917a2f02dcf3b
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-05-16 19:00:49 +0530

    Create Q19.java

---
+++ Q19.java
@@ -0,0 +1,10 @@
+
+public class Q19 {
+    public static void main(String[] args) {
+        //write your code here
+        String name ="Sam" ;
+        String city = "hyd";
+        int age = 19;
+        
+    }
+}
