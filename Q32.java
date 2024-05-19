bec67d0e19948f0845218a787b866408cc06f6bf
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-05-20 01:29:51 +0530

    Create Q32.java

---
+++ Q32.java
@@ -0,0 +1,24 @@
+/*Create sqr and cube methods that take an integer (int) and return its square and cube, respectively.
+Methods must be public and static, with an int return type.
+Method main is not involved in testing.
+Requirements:
+•	The Solution class must have public static sqr(int) method, which returns a square of a number (type int).
+•	The Solution class must have public static cube(int) method, which returns a cube of a number (type int). */
+/* 
+Square and cube
+*/
+
+public class Q32 {
+    public static void main(String[] args) {
+        int number = 3;
+        System.out.printf("%d - %d - %d", number, sqr(number), cube(number));
+    }
+
+    //write your code here
+    public static int cube(int n){
+        return n*n*n;
+    }
+    public static int sqr(int n){
+        return n*n;
+    }
+}
