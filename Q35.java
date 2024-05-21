6ea82baea68ca3bea75782b0a1b1622b2cae2e4c
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-05-21 19:50:44 +0530

    Update Q35.java

--- Q35.java
+++ Q35.java
@@ -8,6 +8,24 @@ In the Client class, declare a String[] variable called socialMedia.
 In the Client class, declare an int variable called age.
 In the Client class, declare an int variable called money.
 In the main method, create a Client object and immediately assign a reference to it to the variable called client.
-Five variables must be declared.
-
+Five variables must be declared.*/
+/* 
+What do we know about our clients?
 */
+
+public class Q35 {
+    public static void main(String[] args) {
+        //write your code here
+        Client client = new Client();
+        
+    }
+
+    public static class Client {
+        //write your code here
+        String name;
+        String[] socialMedia ;
+        int age ;
+        int money;
+        
+    }
+}
