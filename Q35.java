e4130d0a68b6d530a55394b557bef55ec1fd6ac1
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-05-21 19:50:01 +0530

    Create Q35.java

---
+++ Q35.java
@@ -0,0 +1,13 @@
+/*In the Client class, declare the following variables: String name, String[] socialMedia, int age, and int money.
+In the main method, create a Client object and store a reference to it in the variable called client.
+Hint: Use the following construct to create a Client object and assign a reference to that object to the variable client:
+VariableType variableName = new TypeOfObjectBeingCreated();
+Requirements:
+In the Client class, declare a String variable called name. 
+In the Client class, declare a String[] variable called socialMedia.
+In the Client class, declare an int variable called age.
+In the Client class, declare an int variable called money.
+In the main method, create a Client object and immediately assign a reference to it to the variable called client.
+Five variables must be declared.
+
+*/
