f7151b0f4e70f7fa54e57e435fb3e22be411e69b
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-05-24 21:36:56 +0530

    Create Sphere.java

---
+++ Sphere.java
@@ -0,0 +1,30 @@
+/*In the Sphere class, create a constructor that initializes all the instance variables.
+The constructor must have four parameters.
+Requirements:
+•	The Sphere class must have four variables called x, y, z and r.
+•	The class must have one constructor.
+•	The constructor must be public.
+•	The constructor must have three int and one double parameters.
+•	The constructor must initialize all of the class's instance variables. */
+
+/* 
+The basis of a ball
+*/
+
+public class Sphere {
+    public int x;
+    public int y;
+    public int z;
+    public double r;
+    
+  public Sphere(int x,int y,int z,double r){
+       this.x = x;
+       this.y = y;
+       this.z = z;
+       this.r = r;
+      }
+    //write your code here
+    public static void main(String[] args) {
+      
+    }
+}
