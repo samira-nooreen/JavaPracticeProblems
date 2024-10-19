f156539020844c5000f80b8d93cd03751af88661
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-10-19 15:36:49 +0530

    Add files via upload

--- SpeedConverter.java
+++ SpeedConverter.java
@@ -1,23 +1,20 @@
 public class SpeedConverter {
 
-
-    public static long toMilesPerHour(double kilometersPerHour){
-        if (kilometersPerHour <  0) {
+    public static long toMilesPerHour(double KilometersPerHour){
+        if (KilometersPerHour < 0 ) {
             return -1;
         }
-
-        return  Math.round(kilometersPerHour/1.609);
-
+        return Math.round(KilometersPerHour/1.609);
     }
-    public static void printConversion(double kilometersPerHour){
-        if (kilometersPerHour < 0){
+
+    public static void printConversion(double KilomertersPerHour){
+        if (KilomertersPerHour < 0 ){
             System.out.println("Invalid Value");
         }else {
-            long milesPerHour = toMilesPerHour(kilometersPerHour);
-            System.out.println(kilometersPerHour +
-                    "km/h = " + milesPerHour +
-                    "mi/h");
+            long milesPerHour = toMilesPerHour(KilomertersPerHour);
+            System.out.println(KilomertersPerHour  +
+                    " Km/h = " + milesPerHour +
+                    " mi/h");
         }
     }
-
 }
