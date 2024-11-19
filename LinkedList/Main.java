bf584d26c856b4ef24dd361731e783ba013f765d
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-11-20 00:16:56 +0530

    Add files via upload

---
+++ LinkedList/Main.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        //Stack
+//        linkedList.push("A");
+//        linkedList.push("B");
+//        linkedList.push("C");
+//        linkedList.push("D");
+//        linkedList.push("F");
+//        linkedList.pop();
+
+        //Queue
+        linkedList.offer("A");
+        linkedList.offer("B");
+        linkedList.offer("C");
+        linkedList.offer("D");
+        linkedList.offer("F");
+//        linkedList.poll();
+
+        linkedList.add(4,"E");
+        linkedList.remove("E");
+
+        System.out.println(linkedList.peekFirst());
+        System.out.println(linkedList.peekLast());
+        linkedList.addFirst("0");
+        linkedList.addLast("G");
+        String first = linkedList.removeFirst();
+        String last = linkedList.removeLast();
+
+//        System.out.println(linkedList.indexOf("F"));
+
+        System.out.println(linkedList);
+    }
+}
