980191b97b354c9317261d2131d72c1af54df37e
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-12-05 00:54:12 +0530

    Create PosiiveNegativeZero.java

---
+++ PosiiveNegativeZero.java
@@ -0,0 +1,49 @@
+/* Positive and Negative
+Write a program to check whether the three given numbers as input are Positive, Negative, or Zero.
+Sample 1:
+Input               Output
+20                  Positive
+0                   Zero  
+-95                 Negative       */
+
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        num = scanner.nextInt(); // input first number
+        // Check first number
+        if (num > 0){
+            System.out.println("Positive");
+        } else if(num < 0 ){
+             System.out.println("Negative");
+        } else{
+             System.out.println("");
+        }
+        
+        
+        
+        num = scanner.nextInt(); // input second number
+        // Check second number
+          if (num > 0){
+            System.out.println("Positive");
+        } else if(num < 0 ){
+             System.out.println("Negative");
+        } else{
+             System.out.println("");
+        }
+        
+        num = scanner.nextInt(); // input third number
+        // Check third number
+         if (num > 0){
+            System.out.println("Positive");
+        } else if(num < 0 ){
+             System.out.println("Negative");
+        } else{
+             System.out.println("");
+        }
+    }
+}
