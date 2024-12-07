899dcc70aecf6c848d32cfdc0c39a82afb4410dc
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-12-08 02:01:02 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-       Cat mycat = new Cat() ;
-       mycat.makeNoise();
+        ArrayList<Integer>items = new ArrayList ();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(5);
+
+       printDoubled(items);
+    }
+
+    private static void printDoubled(ArrayList<Integer> n){
+        for (Object i : n){
+            System.out.println((Integer) i* 2);
+        }
     }
 }
