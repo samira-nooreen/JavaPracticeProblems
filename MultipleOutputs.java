629447d51b06523797e03210fdfe45591a781afa
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-12-18 01:08:14 +0530

    Create MultipleOutputs.java

---
+++ MultipleOutputs.java
@@ -0,0 +1,21 @@
+/* Multiple Outputs
+We can add as many println statements as we want.
+
+Task
+Write a program which does the following:
+
+Add a println statement and output the sum of 3 + 4.
+Add another println statement and output the sum of 2 + 1.
+Note - Notice that the output is printed on separate lines. 
+*/
+
+class CodeChef
+{
+	public static void main (String[] args)
+	{
+		// add two println statements here
+		System.out.println(3+4);
+		System.out.println(2+1);
+		
+	}
+}
