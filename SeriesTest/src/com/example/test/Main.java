56877f398b027056982f6557f3a022eb4045b343
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-12-20 00:32:27 +0530

    Add files via upload

--- SeriesTest/src/com/example/test/Main.java
+++ SeriesTest/src/com/example/test/Main.java
@@ -1,12 +1,20 @@
 
 package com.example.test;
 
-        import com.sam.myLib.Series;
+        import com.sam.MyLib.Series;
 
 public class Main {
     public static void main(String[] args) {
         for (int i = 0 ; i <= 10 ; i++){
             System.out.println(Series.nSum(i));
+        }
+        System.out.println("**************");
+        for (int i = 0 ; i <= 10 ; i++){
+            System.out.println(Series.factorial(i));
+        }
+        System.out.println("**************");
+        for (int i = 0 ; i <= 10 ; i++){
+            System.out.println(Series.fibonnaci(i));
         }
     }
 }
