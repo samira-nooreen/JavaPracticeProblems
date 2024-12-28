f2f497044d3b312df53a4d25e5ba0076f4ff6ac5
Samira <132845068+samira-nooreen@users.noreply.github.com>
2024-12-29 01:50:05 +0530

    Add files via upload

--- LearnMap.java
+++ LearnMap.java
@@ -1,16 +1,19 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class LearnMap {
     public static void main(String[] args) {
 
-        Map<String , Integer> numbers = new HashMap<>();
+        Map<String , Integer> numbers = new TreeMap<>();
 
         numbers.put("One" , 1);
         numbers.put("Two" , 2);
         numbers.put("Three" , 3);
         numbers.put("Four" , 4);
         numbers.put("Five" , 5);
+
+        numbers.remove("Three",3);
 
 
 //        if (numbers.containsKey("Two")){
